1766a63462caee90ccf172ede3a85c704090ddd2
Mark A. Miller <mamillerpa@gmail.com>
2020-05-24 13:14:51 -0400

    almost half comments!

--- rxnav_med_mapping_tuneup_followon.R
+++ rxnav_med_mapping_tuneup_followon.R
@@ -1,12 +1,6 @@
 library(jsonlite)
 library(fields)
 
-# why the difference in row counts?
-# > dim(unknowns.approximate.original)
-# [1] 95347    22
-# > dim(unknowns.approximate.original.dists)
-# [1] 95230    34
-
 # features
 stringdist.cols <- c("lv",
                      "lcs",
@@ -78,6 +72,7 @@ caret::findCorrelation(
   names = TRUE,
   exact = TRUE
 )
+
 # All correlations <= 0.9
 # character(0)
 # keep all, provided the importances are high
@@ -115,93 +110,6 @@ target.col <- "RELA"
 
 target <- downsampled[, target.col]
 
-# # 4000
-# [1] 17
-# [1] "2020-05-13 12:05:20 EDT"
-# [1] "2020-05-13 12:28:38 EDT"
-# user   system  elapsed
-# 1384.452    8.197 1397.734
-# [1] 16
-# [1] "2020-05-13 12:28:38 EDT"
-# [1] "2020-05-13 12:50:30 EDT"
-# user   system  elapsed
-# 1299.619    6.983 1310.771
-# [1] 15
-# [1] "2020-05-13 12:50:30 EDT"
-# [1] "2020-05-13 13:11:31 EDT"
-# user   system  elapsed
-# 1247.814    7.791 1260.578
-# [1] 14
-# [1] "2020-05-13 13:11:31 EDT"
-# [1] "2020-05-13 13:31:32 EDT"
-# user   system  elapsed
-# 1187.588    7.245 1199.828
-# [1] 13
-# [1] "2020-05-13 13:31:32 EDT"
-# [1] "2020-05-13 13:50:07 EDT"
-# user   system  elapsed
-# 1103.804    6.548 1114.624
-# [1] 12
-# [1] "2020-05-13 13:50:07 EDT"
-# [1] "2020-05-13 14:07:05 EDT"
-# user   system  elapsed
-# 1009.865    4.391 1017.123
-# [1] 11
-# [1] "2020-05-13 14:07:05 EDT"
-# [1] "2020-05-13 14:23:41 EDT"
-# user  system elapsed
-# 982.901   7.668 994.674
-# [1] 10
-# [1] "2020-05-13 14:23:41 EDT"
-# [1] "2020-05-13 14:39:03 EDT"
-# user  system elapsed
-# 910.406   6.301 921.402
-# [1] 9
-# [1] "2020-05-13 14:39:03 EDT"
-# [1] "2020-05-13 14:53:05 EDT"
-# user  system elapsed
-# 833.416   4.528 841.259
-# [1] 8
-# [1] "2020-05-13 14:53:05 EDT"
-# [1] "2020-05-13 15:05:42 EDT"
-# user  system elapsed
-# 749.843   4.004 755.946
-# [1] 7
-# [1] "2020-05-13 15:05:42 EDT"
-# [1] "2020-05-13 15:17:18 EDT"
-# user  system elapsed
-# 688.727   3.646 694.262
-# [1] 6
-# [1] "2020-05-13 15:17:18 EDT"
-# [1] "2020-05-13 15:28:15 EDT"
-# user  system elapsed
-# 646.817   6.133 657.145
-# [1] 5
-# [1] "2020-05-13 15:28:15 EDT"
-# [1] "2020-05-13 15:37:41 EDT"
-# user  system elapsed
-# 558.833   4.007 565.101
-# [1] 4
-# [1] "2020-05-13 15:37:41 EDT"
-# [1] "2020-05-13 15:45:56 EDT"
-# user  system elapsed
-# 487.973   3.818 493.927
-# [1] 3
-# [1] "2020-05-13 15:45:56 EDT"
-# [1] "2020-05-13 15:52:59 EDT"
-# user  system elapsed
-# 419.770   3.460 422.898
-# [1] 2
-# [1] "2020-05-13 15:52:59 EDT"
-# [1] "2020-05-13 15:58:31 EDT"
-# user  system elapsed
-# 326.352   3.166 331.206
-# [1] 1
-# [1] "2020-05-13 15:58:31 EDT"
-# [1] "2020-05-13 16:01:09 EDT"
-# user  system elapsed
-# 153.595   1.954 156.687
-
 max.ntry <- ncol(features) - 1
 min.ntry <- 5
 static.ntree <- 601
@@ -237,16 +145,11 @@ just.oobs <- lapply(err.vs.mtry, function(current.mtry) {
 })
 
 just.oobs <- do.call(cbind, just.oobs)
-# rows = ntree 1 to max
-# columns = mtry max to 1
+
 just.oobs <- just.oobs[, ncol(just.oobs):1]
 
 temp <- log10(just.oobs)
 temp[temp == -Inf] <- NA
-
-# colnames(temp) <- 
-
-# image.plot(temp)
 
 # set these high enough that the plot doens't show the edges with dramatically highest errors
 # not cheating... just good visualizastion
@@ -280,8 +183,6 @@ axis(
   labels = as.character(min.try:max.ntry)
 )
 
-# use 375 * 12 ? try again after picking top 13 featuers
-
 importances <- err.vs.mtry[["11"]]$importance
 
 importances <-
@@ -295,4 +196,12 @@ importances <- importances[order(importances$rel.importance), ]
 
 plot(importances$MeanDecreaseGini)
 
-# # how to find an inflection point programatically
+# how to find an inflection point programatically
+
+# why the difference in row counts?
+# > dim(unknowns.approximate.original)
+# [1] 95347    22
+# > dim(unknowns.approximate.original.dists)
+# [1] 95230    34
+
+
