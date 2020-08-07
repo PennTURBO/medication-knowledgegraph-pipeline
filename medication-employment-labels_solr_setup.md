575b293d5028ea749253ea31785bcca3050ce928
Mark A. Miller <mamillerpa@gmail.com>
2020-08-06 21:28:21 -0400

    single Solr script; started documentation

---
+++ medication-employment-labels_solr_setup.md
@@ -0,0 +1,121 @@
+## Ensure that Solr is installed & a core has been created
+
+TODO: Solr's role within medication mapping, download URL, basic requirements
+
+
+
+Solr should be running on the same computer that will be running the TURBO medication mapping knowledgebase build code, or it can run on another computer as long as the necessary TCP port is open.
+
+
+
+The PennTURBO team is currently using Solr 8.5.1. We have also had good results with late 7.x versions.
+
+
+
+Oracle Corporation OpenJDK 64-Bit Server VM 1.8.0_252 25.252-b09
+
+
+
+Some of these steps may require that the user has administrative privileges on the Solr server. For example, running the Solr process and writing to the Solr data path.
+
+
+
+Solr has a rest API as well as a web console. Start by  checking if the Solr server is running. One way is visiting 
+
+
+
+`http://<solr server address>:<Solr port>`
+
+Where the default port is 8983
+
+If the Solr web console doesn't appear, you could also run
+
+
+
+`ps -ef | grep Solr` 
+
+
+
+on a Unix-like system
+
+
+
+or 
+
+
+
+`<Solr home>/bin/Solr status`
+
+
+
+The PennTURBO team has been using `/opt/Solr` as the Solr home.
+
+
+
+If it is not running, you will need to decide whether to run Solr in cloud mode. The PennTURBO team has been running Solr in standalone mode, not cloud mode. To start Solr in standalone mode, run a command lie this
+
+
+
+`<Solr home>/bin/Solr start`
+
+Common arguments to this command include increasing the Java heap memory and specifying the Solr data path
+
+
+
+-Dsolr.solr.home=/var/Solr/data
+
+-Xmx512m
+
+
+
+In Solr standalone mode, documents are indexed into databases called cores. Therefore, ensure that the core specified in CONFIGURATION FILE creates. That can be done by clicking on the "select a n option" button on the Solr web console's home page, or by pining over REST/CURL etc.
+
+
+
+`http://localhost:8983/solr/<core name>/ping`
+
+
+
+If the core does not exist, create it with a command like this
+
+`<Solr home>/bin/Solr create -c <core name>`
+
+
+
+----
+
+
+
+rxnav_med_mapping_load_materialize_etc.R
+
+
+
+List libraries
+
+
+
+bootstraps the configuration from a GitHub Gist at https://gist.github.com/turbomam/f082295aafb95e71d109d15ca4535e46
+
+the devtools library must be loaded in advance
+
+R is able to check the SHA1 sum of Gists. The PennTURBO team has been using this feature. 
+
+>  Error: SHA-1 hash of downloaded
+
+Indicates may indicate that the gist has been modified since the last modification to `rxnav_med_mapping_solr_upload_post.R`. The user should determine if they trust the modified gist. If so, update the SHA1 argument passed to `source_gist`
+
+TODO: move gist from Mark's personal repo and into a PennTURBO repo.
+
+This script currently requires a properly formatted `turbo_R_setup.yaml` in the home directory of the user who started this script
+
+see https://gist.github.com/turbomam/a3915d00ee55d07510493a9944f96696 for template
+
+TODO: think of a better place to keep  `turbo_R_setup.yaml` , or even make other changes to the config/setup approach
+
+
+
+  `turbo_R_setup.yaml` has settings for 
+
+- the GraphDB triplestore form which medication knowledge will be retrieved
+- the Solr server and core to which it will be sent, as a Solr-ready JSON file
+- ssh and filesystem details, which are used for moving the JSON file to a server from which it can be posted to the Solr server. requires ssh public key (passwordless) login
---
+++ pipeline/rxnav_med_mapping_solr_build_upload_post.R
@@ -0,0 +1,232 @@
+medlabels_solr_jason_path <- "medlabels_for_chebi_for_solr.json"
+
+library(devtools)
+library(ssh)
+
+# requires a properly formatted "turbo_R_setup.yaml" in the home directory of the user who started this script
+# see https://gist.github.com/turbomam/a3915d00ee55d07510493a9944f96696 for template
+devtools::source_gist(id = "https://gist.github.com/turbomam/f082295aafb95e71d109d15ca4535e46",
+                      sha1 = "dbc656aaf63b23dfdd35d875f6772e7c468170a4",
+                      filename = "turbo_R_setup.R")
+
+# MAIN labels
+# < 1 minute
+system.time(pre.main.solr.res <-
+              q2j2df(query = config$main.solr.query))
+
+main.solr.res <-
+  pre.main.solr.res[, c("mediri", "definedin",  "employment", "medlabel")]
+
+names(main.solr.res) <-
+  c("id", "definedin",  "employment", "main.label")
+
+# clinrel structctclass labels
+system.time(crsc.solr.res <-
+              q2j2df(query = config$clinrel_structclass.solr))
+
+crsc.addtions <- setdiff(crsc.solr.res$id, main.solr.res$id)
+
+colnames(crsc.solr.res) <-
+  c("main.label", "definedin", "id", "employment")
+
+crsc.solr.res <-
+  crsc.solr.res[crsc.solr.res$id %in% crsc.addtions, ]
+
+main.solr.res <-
+  rbind.data.frame(main.solr.res, crsc.solr.res[, colnames(main.solr.res)])
+
+### DrOn's labels for ChEBI
+system.time(
+  dron.additional.chebi.labels <-
+    q2j2df(query = config$dron.additional.chebi.label.query)
+)
+
+colnames(dron.additional.chebi.labels) <-
+  c('id', 'dron.for.chebi', 'dtich')
+
+temp <- table(dron.additional.chebi.labels$id)
+temp <- cbind.data.frame(names(temp), as.numeric(temp))
+colnames(temp) <- c("id", "count")
+temp <- temp$id[temp$count == 1]
+dron.additional.chebi.labels <-
+  dron.additional.chebi.labels[dron.additional.chebi.labels$id %in% temp , c("id", "dron.for.chebi")]
+
+merged <-
+  dplyr::full_join(main.solr.res, dron.additional.chebi.labels)
+
+#### All RxNorm alternative labels
+# BROKEN as of 2020AA?
+
+system.time(rxn.alt.lab.solr.res <-
+              q2j2df(query = config$rxn.alt.lab.solr.query))
+
+colnames(rxn.alt.lab.solr.res) <- c("rxn.alt",  "id")
+
+temp <- colnames(rxn.alt.lab.solr.res)
+
+rxn.alt.lab.solr.res <-
+  aggregate(rxn.alt ~ id,
+            data = rxn.alt.lab.solr.res,
+            paste,
+            collapse = "|")
+
+merged <-
+  dplyr::full_join(merged, rxn.alt.lab.solr.res)
+
+#### chebi synonyms
+system.time(chebi.synonym.res <-
+              q2j2df(query = config$chebi.synonym.query))
+
+colnames(chebi.synonym.res) <-
+  c("chebi.syn",
+    "synstrength",
+    "synlen",
+    "syntype",
+    "id",
+    "synsource",
+    "employment" ,
+    "l")
+
+chebi.synonym.res$synsource <-
+  sub(
+    pattern = 'http://purl.obolibrary.org/obo/chebi#',
+    replacement = '',
+    x = chebi.synonym.res$synsource,
+    fixed = TRUE
+  )
+
+chebi.synonym.res$syntype <-
+  sub(
+    pattern = 'http://purl.obolibrary.org/obo/chebi#',
+    replacement = '',
+    x = chebi.synonym.res$syntype,
+    fixed = TRUE
+  )
+
+chebi.synonym.res$synstrength <-
+  sub(
+    pattern = 'http://www.geneontology.org/formats/oboInOwl#',
+    replacement = '',
+    x = chebi.synonym.res$synstrength,
+    fixed = TRUE
+  )
+
+chebi.selected.syns <-
+  unique(chebi.synonym.res[(!(is.na(chebi.synonym.res$syntype)) &
+                              chebi.synonym.res$syntype == 'BRAND_NAME') |
+                             chebi.synonym.res$synsource %in% c('KEGG_COMPOUND', 'KEGG_DRUG', 'DrugBank', 'UniProt') |
+                             chebi.synonym.res$employment == 'http://example.com/resource/clinrel_structclass' ,
+                           c("id", "chebi.syn")])
+
+colnames(chebi.selected.syns) <- c("id", "synalt")
+
+chebi.selected.syns$synalt <-
+  as.character(chebi.selected.syns$synalt)
+
+temp <- colnames(chebi.selected.syns)
+
+chebi.selected.syns <-
+  aggregate(synalt ~ id, data = chebi.selected.syns, paste, collapse = "|")
+
+merged <-
+  dplyr::full_join(merged, chebi.selected.syns)
+
+merged <- merged[order(merged$id), ]
+
+merged$chebi.demoted <- NA
+merged$chebi.demoted[!is.na(merged$dron.for.chebi)] <-
+  merged$main.label[!is.na(merged$dron.for.chebi)]
+merged$main.label[!is.na(merged$dron.for.chebi)] <-
+  merged$dron.for.chebi[!is.na(merged$dron.for.chebi)]
+
+####
+
+system.time(chebi.chebi.role.syns <-
+              q2j2df(query = config$chebi.role.syns))
+
+names(chebi.chebi.role.syns) <- c("id", "rolesyn")
+
+agg <-
+  aggregate(rolesyn ~ id, data = chebi.chebi.role.syns, paste, collapse = "|")
+
+merged <-
+  dplyr::full_join(merged, agg)
+
+####
+
+listed <-
+  apply(
+    X = merged,
+    MARGIN = 1,
+    FUN = function(currentrow) {
+      print(currentrow[['id']])
+      tokens <-
+        c(currentrow[["main.label"]],
+          currentrow[["dron.for.chebi"]],
+          currentrow[["rxn.alt"]],
+          currentrow[["synalt"]],
+          currentrow[["rolesyn"]],
+          currentrow[["chebi.demoted"]])
+      tokens <- unlist(tokens)
+      tokens <- unlist(strsplit(tokens, " |\\|"))
+      tokens <- sort(unique(tolower(tokens)))
+      # print(tokens)
+      return(
+        list(
+          id = currentrow[['id']],
+          medlabel = tolower(currentrow[['main.label']]),
+          tokens = tokens,
+          definedin = currentrow[['definedin']],
+          employment = currentrow[['employment']]
+        )
+      )
+    }
+  )
+names(listed) <- merged$id
+
+temp <- listed
+names(temp) <-  NULL
+temp <- toJSON(temp, pretty = TRUE)
+
+write_lines(temp, path = medlabels_solr_jason_path)
+
+# mm.kb.solr.client <-
+mm.kb.solr.client <- SolrClient$new(
+  host = config$med.map.kb.solr.host,
+  path = "search",
+  port = config$med.map.kb.solr.port
+)
+
+# could also ping it
+print(mm.kb.solr.client)
+
+# clear the core!
+mm.kb.solr.client$delete_by_query(name = config$med.map.kb.solr.core, query = "*:*")
+
+# https://debian-administration.org/article/530/SSH_with_authentication_key_instead_of_password
+session <- ssh_connect(paste0(config$ssh.user,"@",config$ssh.host))
+#' @markampa@turbo-prd-app01.pmacs.upenn.edu")
+print(session)
+
+# countdown dips to negative before finishing
+scp_upload(
+  session = session,
+  files = paste0(config$json.source, config$json.for.solr),
+  to =  paste0(config$json.dest, config$json.for.solr),
+  verbose = TRUE
+)
+
+ssh.command <-
+  paste0(
+    "curl 'http://localhost:8983/solr/",
+    config$med.map.kb.solr.core,
+    "/update?commit=true&overwrite=false' --data-binary  @",
+    config$json.dest, config$json.for.solr,
+    " -H 'Content-type:application/json'"
+  )
+
+print(ssh.command)
+
+out <- ssh_exec_wait(session, command = ssh.command)
+
+print(out)
