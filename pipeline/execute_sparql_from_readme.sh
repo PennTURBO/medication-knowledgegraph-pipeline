6c374abb1d08c3d4007fc5686a7e1aa09e32f375
Mark A. Miller <mamillerpa@gmail.com>
2020-08-11 16:23:38 -0400

    wrote and ran sparql from readme

---
+++ pipeline/execute_sparql_from_readme.sh
@@ -0,0 +1,13 @@
+robot \
+query \
+--input ../ontology/tmm_ontology.ttl \
+--query config/Validating_a_Solr_collection.rq build/Validating_a_Solr_collection.csv
+
+
+robot \
+query \
+--input ../ontology/tmm_ontology.ttl \
+--query config/Selecting_queries_based_on_employment.rq build/Selecting_queries_based_on_employment.csv
+
+
+# https://github.com/PennTURBO/medication-knowledgegraph-pipeline/blob/master/ontology/readme.md
